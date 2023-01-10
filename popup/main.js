86c72deffc7dc7de5d6eb4aa27fa94d29758dd18
kamiada <kamiada528@gmail.com>
2023-01-10 22:05:24 +0000

    styling, a bit of functionality

--- popup/main.js
+++ popup/main.js
@@ -3,15 +3,22 @@ window.onload=function(){
   const saveButton = document.getElementById("saveButton");
   const addProjectBtn = document.getElementById("addProject");
 
+  saveButton.addEventListener("click", saveProject);
   addProjectBtn.addEventListener("click", hideSection);
-  saveButton.addEventListener("click", save);
 }
-function save() {
-  console.log("works");
-}
+
 function hideSection() {
   if($("#section_no_projects") != null) {
     $("#section_no_projects").hide("slow","swing");
     $("#section_newProject").show("slow","swing");
   }
+}
+
+function saveProject() {
+  let getTitle = document.getElementById("title").value;
+  let getGoal = document.getElementById("goal").value;
+
+  chrome.storage.local.set({
+    'title': title
+  });
 }
