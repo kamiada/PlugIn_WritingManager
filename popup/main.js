ee707d29efcfc607dc6c10cb81ae6273e205c5d4
kamiada <kamiada528@gmail.com>
2023-01-10 21:02:06 +0000

    renaming

--- popup/main.js
+++ popup/main.js
@@ -1,28 +1,16 @@
-// const primarySection = document.querySelector(".empty_section");
-// function addFirstProject() {
-//     primarySection.setAttribute("display", "none");
-//     console.log('works');
-// }
 
-// $(".link_addProject").click(function() {
-//   $("#empty_section").hide();
-// });
 window.onload=function(){
   const saveButton = document.getElementById("saveButton");
-  const emptyStateSection = document.getElementById("empty_section");
-  const primaryButton = document.getElementById("primary");
+  const addProjectBtn = document.getElementById("addProject");
 
-  primaryButton.addEventListener("click", hideSection);
+  addProjectBtn.addEventListener("click", hideSection);
   saveButton.addEventListener("click", save);
 }
-// document.getElementById("saveButton").addEventListener("click", save);
 function save() {
   console.log("works");
 }
 function hideSection() {
-  if($("#empty_section") != null) {
-    $("#empty_section").hide();
-    console.log('here');
+  if($("#first_section_no_projects") != null) {
+    $("#first_section_no_projects").hide();
   }
-  console.log("empty");
 }
