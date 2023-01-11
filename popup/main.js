feafce433fc57ad4c5b66c605624c7c72d297d20
kamiada <kamiada528@gmail.com>
2023-01-11 10:57:28 +0000

    working on 3rd section

--- popup/main.js
+++ popup/main.js
@@ -4,21 +4,28 @@ window.onload=function(){
   const addProjectBtn = document.getElementById("addProject");
 
   saveButton.addEventListener("click", saveProject);
-  addProjectBtn.addEventListener("click", hideSection);
+  addProjectBtn.addEventListener("click", hideFirstSection);
 }
 
-function hideSection() {
+function hideFirstSection() {
   if($("#section_no_projects") != null) {
     $("#section_no_projects").hide("slow","swing");
     $("#section_newProject").show("slow","swing");
   }
 }
 
+function hideSecondSection() {
+  if($("#section_newProject") != null) {
+    $("#section_newProject").hide("slow","swing");
+    $("#section_allProjects").show("slow","swing");
+  }
+}
+
 function saveProject() {
   let getTitle = document.getElementById("title").value;
   let getGoal = document.getElementById("goal").value;
-
+  hideSecondSection();
   chrome.storage.local.set({
-    'title': title
+    'title': getTitle
   });
 }
