6379b43f2123cf9fc4879885fe08213c23200f63
kamiada <kamiada528@gmail.com>
2023-03-16 20:15:24 +0000

    weeeelll working on it

--- popup/main.js
+++ popup/main.js
@@ -1,66 +1,64 @@
+let output = document.getElementById("output");
 
-window.onload=function(){
+window.onload = function () {
   const saveButton = document.getElementById("saveButton");
   const addProjectBtn = document.getElementById("addProject");
 
-  let projectTitle = document.getElementById("title");
-
   saveButton.addEventListener("click", saveProject);
   addProjectBtn.addEventListener("click", hideFirstSection);
-
-  browser.storage.local.get('title', function(result) {
-    if (result.title) {
-      projectTitle.value = result.title;
-    }
-  })
-}
+};
 
 function hideFirstSection() {
-  if($("#section_no_projects") != null) {
-    $("#section_no_projects").hide("slow","swing");
-    $("#section_newProject").show("slow","swing");
+  if ($("#section_no_projects") != null) {
+    $("#section_no_projects").hide("slow", "swing");
+    $("#section_newProject").show("slow", "swing");
   }
 }
 
 function hideSecondSection() {
-  if($("#section_newProject") != null) {
-    $("#section_newProject").hide("slow","swing");
-    $("#section_allProjects").show("slow","swing");
-  }
-}
-
-function getValues() {
-  let title2 = $("title").val();
-  console.log(title2);
-  if($("#title").val() != null || $("#title").val() != '') {
-    let title = $("#title").val();
-    chrome.storage.local.set({
-      'title' : title
-    });
-    // check();
+  if ($("#section_newProject") != null) {
+    $("#section_newProject").hide("slow", "swing");
+    $("#section_allProjects").show("slow", "swing");
   }
 }
 
 function saveProject() {
-  getValues();
+  if ($("#title").val() != null || $("#title").val() != "") {
+    let title = $("#title").val();
+    chrome.storage.local.set({
+      title: title,
+    });
+  }
+  // check();
+  updateListOfProjects();
   hideSecondSection();
 }
 
 function check() {
-    var res = "";
-    chrome.storage.local.get('title', function (result) {
-      console.log(result);
-        res = result.title;
-        alert(result.title);
-        $("#title").val(res);
-    });
+  var res = "";
+  chrome.storage.local.get("title", function (result) {
+    console.log(result);
+    res = result.title;
+    alert(result.title);
+    $("#title").val(res);
+  });
 }
 
-function updateProject() 
- {
+function updateListOfProjects() {
+  const sectionWithProjects = document.getElementById("section_allProjects");
+  chrome.storage.local.get("title", function (result) {
+    res = result.title;
+    if (res!= null) {
+      output.textContent = $("#title").val(res);
+      output.textContent = res;
+    }
+  });
+}
+
+function updateProject() {
   let data = {
     text: input1.value,
-    number: input2.value
+    number: input2.value,
   };
   browser.storage.local.set(data);
 }
